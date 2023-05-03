1030e56fe3c5f743738788c9d1a75e47a8fd81b2
AngelManuelLibrerosRoman <l21021342@veracruz.tecnm.mx>
2023-05-03 03:54:33 -0600

    Proyecto Conexion Escolar 1.0
    Elaborado por: Integrantes del equipo

--- src/main/java/modelo/ModeloAlumno.java
+++ src/main/java/modelo/ModeloAlumno.java
@@ -1,51 +1,5 @@
 package modelo;
 
 public class ModeloAlumno {
-    private int numControl;
-    private String nombre;
 
-    private int especialidad;
-    public ModeloAlumno(){
-
-    }
-
-
-    public ModeloAlumno(int numControl, String nombre, int especialidad) {
-        this.numControl = numControl;
-        this.nombre = nombre;
-        this.especialidad = especialidad;
-    }
-
-    public int getNumControl() {
-        return numControl;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setNumControl(int numControl) {
-        this.numControl = numControl;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setEspecialidad(int especialidad) {
-        this.especialidad = especialidad;
-    }
-
-    @Override
-    public String toString() {
-        return "ModeloAlumno{" +
-                "numControl=" + numControl +
-                ", nombre='" + nombre + '\'' +
-                ", especialidad=" + especialidad +
-                '}';
-    }
 }
